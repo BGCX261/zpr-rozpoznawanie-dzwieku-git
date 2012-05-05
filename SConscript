22dd4012de78a5650e0a1f1248e3596f42e5f0bf
rychterl@gmail.com <rychterl@gmail.com>
2012-05-05 18:46:15 +0200

    Dostosowanie do działania na linuxie

--- SConscript
+++ SConscript
@@ -1,4 +1,7 @@
 env = Environment()
-env['PCHSTOP'] = 'stdafx.h' # indicates header used to generate precompiled header file (VC++ only, ignored otherwise)
-env['PCH'] = env.PCH('stdafx.cpp')[0] # generates precompiled header file (VC++ only, ignored otherwise)
+
+if hasattr(env, "PCH"):
+	env['PCHSTOP'] = 'stdafx.h'  #indicates header used to generate precompiled header file (VC++ only, ignored otherwise)
+	env['PCH'] = env.PCH('stdafx.cpp')[0]  #generates precompiled header file (VC++ only, ignored otherwise)
+
 env.Program(target = '../ZPR', source = ["main.cpp"])
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,21 +1,29 @@
-#include "stdafx.h"
+ï»¿#include "stdafx.h"
 
 using namespace std;
 
 int main()
 {
-	locale myloc ("Polish");
-	locale oldloc = locale::global(myloc);
+#ifdef _WIN32
+	locale oldloc;
+	try
+	{
+		locale myloc("Polish");
+		oldloc = locale::global(myloc);
+	}
+	catch(...) {}
+#endif
 
-	cout << "Projekt ZPR pod kierownictwem dr in¿. Rafa³a Biedrzyckiego:" << endl;
-	cout << "\"Zaimplementowaæ algorytm indukcji klasyfikatora bazuj¹cy na sieci neuronowej\"" << endl;
 	cout << endl;
-	cout << "Autorzy: £ukasz Rychter, Maciej Sikora" << endl;
+	cout << "Projekt ZPR pod kierownictwem dr inÅ¼. RafaÅ‚a Biedrzyckiego:" << endl;
+	cout << "\"ZaimplementowaÄ‡ algorytm indukcji klasyfikatora bazujÄ…cy na sieci neuronowej\"" << endl;
+	cout << endl;
+	cout << "Autorzy: Åukasz Rychter, Maciej Sikora" << endl;
 	cout << endl;
 	
-	locale::global (oldloc);
-
+#ifdef _WIN32
 	system("PAUSE");
+#endif
 
 	return 0;
 }
