8246c72bc2fc709c6b9e8f08ed9faa4934eb7091
Maciej Sikora <sikora.maciek@gmail.com>
2012-06-02 12:08:40 +0200

    Dodany szablon unit testów z pełnymi ustawieniami ścieżek. W przypadku dodania nowego testu, trzeba stworzyć nowy projekt w sources/test oraz przekopiować ustawienia z szablonu. [WAŻNE] Trzeba ściągnąć boosta w wersji 1.46.1, rozpakować archiwum do C:/ (czyli mamy folder C:/boost_1_46_1), wejść do C:/boost_1_46_1, w konsoli odpalić bootstrap.bat, a potem .\bjam - budowanie trwa długo, ale przynajmniej ta wersja się buduje i potem działają unit testy.

---
+++ sources/tests/TestTemplate/TestTemplate.cpp
@@ -0,0 +1,7 @@
+#define BOOST_TEST_MODULE simpe_template test
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(TestTest)
+{
+	BOOST_CHECK_EQUAL(1, 1);
+}
---
+++ sources/tests/WaveReaderTest/WaveReaderTest.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE wave_reader test
+#include <boost/test/unit_test.hpp>
+#include "waveReader.h"
+
+// TODO
+BOOST_AUTO_TEST_CASE(TestTest)
+{
+	BOOST_CHECK_EQUAL(1, 1);
+}
