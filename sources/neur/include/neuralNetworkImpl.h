955b8db9446f1c4727c906f6e72b2963481935ea
Łukasz Rychter <rychterl@gmail.com>
2012-06-09 19:11:08 +0200

    Dodany brakujący neuralNetworkImpl.h

---
+++ sources/neur/include/neuralNetworkImpl.h
@@ -0,0 +1,21 @@
+/** 
+* @file
+* Implementation of Network class
+* 
+* It represents whole neural network structure \n
+* Provides interface to control the whole neural network
+*/
+
+/// Lukasz Rychter
+/// Maciej Sikora
+
+#ifndef __NEURAL_NETWORK_IMPL_H__
+#define __NEURAL_NETWORK_IMPL_H__
+
+template <typename LABEL>
+void NeuralNetwork<LABEL>::initializeNetwork(unsigned long inputNeuronsNum, const std::auto_ptr< std::vector<LABEL> >& outputCategories)
+{
+	//TODO
+}
+
+#endif
